362f86c2695890b58c9f07478b8596860b0868fb
Pekka Kaitaniemi <pekka.kaitaniemi@tocoman.com>
2023-10-01 17:30:17 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let pkgs = import <nixpkgs> {};
     stdenv = pkgs.stdenv;
 in rec {
     leanheatEnv = stdenv.mkDerivation rec {
-        name = "clj-histogram-dev";
+        name = "clj-git-dev";
         version = "1.0.0";
 #        src = ./.;
         buildInputs = [ pkgs.openjdk
@@ -10,7 +10,7 @@ in rec {
                         pkgs.clj-kondo
                         pkgs.leiningen
                       ];
-        EDITOR="vim";
+        EDITOR="emacs";
         _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java1d.xrender=true";
     };
 }
